911a0df4d56531424d991caae3a8268a106a3d3c
sir3n-sn <sir3n.sn@gmail.com>
2018-01-09 13:08:04 +0300

    [Feature #153727979] Added tests for login

--- hc/accounts/tests/test_login.py
+++ hc/accounts/tests/test_login.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.core import mail
 from django.test import TestCase
 from hc.api.models import Check
+from hc.accounts.models import Profile
 
 
 class LoginTestCase(TestCase):
@@ -16,16 +17,16 @@ class LoginTestCase(TestCase):
 
         form = {"email": "alice@example.org"}
 
-        r = self.client.post("/accounts/login/", form)
-        assert r.status_code == 302
+        create_user = self.client.post("/accounts/login/", form)
+        assert create_user.status_code == 302
 
         ### Assert that a user was created
-
+        assert Profile.objects.select_related("user").get(user__email=form["email"])  # Go to database and retrieve user
         # And email sent
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, 'Log in to healthchecks.io')
         ### Assert contents of the email body
-
+        self.assertIn("please open the link below", str(mail.outbox[0].body))
         ### Assert that check is associated with the new user
 
     def test_it_pops_bad_link_from_session(self):
@@ -34,4 +35,19 @@ class LoginTestCase(TestCase):
         assert "bad_link" not in self.client.session
 
         ### Any other tests?
+    def test_invalid_email_during_login_should_not_be_accepted(self):
+        """
+        app should not accept an improper email when logging in
+        :return: redirect back to the login page
+        """
+        check = Check()
+        check.save()
 
+        session = self.client.session
+        session["welcome_code"] = str(check.code)
+        session.save()
+
+        form = {"email": "I_AM_PRETENDING_TO_BE_AN_EMAIL"}
+
+        invalid_email_login = self.client.post("/accounts/login/", form)
+        self.assertTrue(invalid_email_login.status_code, 200)  # returns status code 200 instead of 301/302
