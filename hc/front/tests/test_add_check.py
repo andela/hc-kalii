2462dc21e1033406fe9117a6b4a3621d6ac8486b
adrian <adrian.sebuliba1@gmail.com>
2018-01-11 14:46:03 +0300

    [Feature  #153727977] Add test that team access works

--- hc/front/tests/test_add_check.py
+++ hc/front/tests/test_add_check.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from hc.api.models import Check
 from hc.test import BaseTestCase
 
@@ -12,4 +13,16 @@ class AddCheckTestCase(BaseTestCase):
         assert Check.objects.count() == 1
 
     ### Test that team access works
-    
+    def test_team_access_works(self):
+        url="/checks/add/"
+        #Bob logs in and adds a new check. Bob is on the team
+        self.client.login(username="bob@example.org", password="password")
+        self.client.post(url)
+
+        # Charlie logins and attempts to add a new check, charlie has no team access
+        self.client.login(username="charlie@example.org", password="password")
+        self.client.post(url)
+
+        user_alice = User.objects.get(email="alice@example.org")
+        #Alice can access the team's check since she is also on the team
+        assert Check.objects.filter(user=user_alice).count() == 1
