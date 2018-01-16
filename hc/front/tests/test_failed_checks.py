cc4c6e41506eec0cf500f34a9317f1571dd00da4
sir3n-sn <sir3n.sn@gmail.com>
2018-01-16 12:40:23 +0300

    [Feature #153727983] Added Test, Logic and url path for failing_checks

---
+++ hc/front/tests/test_failed_checks.py
@@ -0,0 +1,20 @@
+from hc.api.models import Check
+from hc.test import BaseTestCase
+from datetime import timedelta
+from django.utils import timezone
+
+
+class MyChecksTestCase(BaseTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.check = Check(user=self.alice, name="Failing checks")
+        self.check.last_ping = timezone.now() - timedelta(days=3)
+        self.check.status = "up"
+        self.check.save()
+
+    def test_unresolved_checks_show_in_endpoint_failed_checks(self):
+        for email in ("alice@example.org", "bob@example.org"):
+            self.client.login(username=email, password="password")
+            unresolved_checks = self.client.get("/failed_checks/")
+            self.assertContains(unresolved_checks, "Failing checks", status_code=200)
