1f71e7672289097625cd768cfc6767d8a3cb65a5
Jakana Kiwanuka <jakanakiwanuka@Jakanas-MacBook-Pro.local>
2018-01-31 14:17:31 +0300

    [Feature #153727992] created models to implment the Blog functionality

--- hc/front/models.py
+++ hc/front/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+class Category(models.Model):
+    '''
+    This model contains the particulars of the category
+    a blog post can belong too
+    '''
+
+    name = models.CharField(max_length=100)
+
+    def ___str__(self):
+        return f"{self.name}"
+
+class Blog_post(models.Model):
+    '''
+    This model contains the particulars of the blog post created
+    '''
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    category = models.ForeignKey(Category, 
+                                on_delete = models.CASCADE)
+    user = models.ForeignKey(User, 
+                            blank=True, 
+                            null=True, 
+                            on_delete= models.CASCADE)
+
+    def ___str__(self):
+        '''Representation of string output from the database'''
+        return f"{self.title}"
+
+class Comment(models.Model):
+    '''
+    This model handles caters to the ability of users to add
+    comments to users blog posts
+    '''
+    comment = models.TextField()
+    blog = models.ForeignKey(Blog_post, blank=True, null=True)
+    user = models.ForeignKey(User, blank=True, null=True,
+                            on_delete=models.CASCADE)
+    published = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.comment}"
+
